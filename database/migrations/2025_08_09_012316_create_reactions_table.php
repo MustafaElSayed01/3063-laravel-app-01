64693637f59a8080ff47fd7982a8ffdb13c1d219
XiuMin <cds.MoustafaElSayed80316@alexu.edu.eg>
2025-08-09 14:23:33 +0300

    added required migrations

---
+++ database/migrations/2025_08_09_012316_create_reactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('reaction_type_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('reaction_type_id')->references('id')->on('reaction_types')->onDelete('cascade');
+            $table->morphs('reactable');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reactions');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -20,44 +20,4 @@ Route::resource('customers', CustomerController::class);
 
 Route::resource('users', UserController::class);
 
-/*Route::prefix('customers')->group(function () {
-    Route::get('/', function () {
-
-        return view('customers.index', compact('customers'));
-    });
-
-    Route::get('{customer}', function ($customer) {
-        return "Customer $customer Page";
-    });
-
-    Route::get('{customer}/edit', function ($customer) {
-        return "Update Customer $customer";
-    });
-
-    Route::get('{customer}/categories/{category}', function ($customer, $category) {
-        return "View $category for Customer $customer";
-    })->whereNumber('customer')->whereAlphaNumeric('category');
-
-    Route::get('{customer}/category_id/{category}', function ($customer, $category) {
-        return "Customer $customer: Categoris with ID $category";
-    })->whereNumber(['customer', 'category']);
-
-    Route::get('branch/{branch}', function ($branch) {
-        return "Customers in $branch Branch";
-    })->whereIn('branch', ['cairo', 'alex', 'giza']);
-
-    Route::get('by-code/{code}', function () {
-        return 'Customers by Code';
-    })->where('code', '[a-z]{3}-[\d]{3}');
-});*/
-
-/*Route::prefix('/users')->group(function () {
-
-    Route::get('/', [UserController::class, 'index']);
-    Route::get('/create', [UserController::class, 'create']);
-    Route::get('/show/{user}', [UserController::class, 'show'])->whereNumber('user');
-    Route::get('/edit/{user}', [UserController::class, 'edit'])->whereNumber('user');
-    Route::get('/delete/{user}', [UserController::class, 'destroy'])->whereNumber('user');
-    Route::get('/role/{role}', [UserController::class, 'role'])->whereIn('role', ['admin', 'user', 'guest', 'manager']);
-});*/
 Route::fallback(fn() => abort(404));
